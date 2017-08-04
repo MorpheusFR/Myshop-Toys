a938bcb9cadc09d684a1d94cc6113fb43143a2be
MorpheusFR <Creatorsk@gmail.com>
2017-08-04 18:31:35 +0300

    add articles.all работа над статьями, ошибки в представлении при выводе статьи

--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from article import views
 from . import views
 
 urlpatterns = [
-    url(r'^1/', views.template_one),#'article.views.template_one'),  # ссылка на ф-цию в представлении
+    #url(r'^1/', views.template_one),#'article.views.template_one'),  # ссылка на ф-цию в представлении
+    url(r'^all/$', views.articles),
+    url(r'^article/get/(?P<article_id>\d+)/$', views.articles), #
+
+
 ]
--- article/views.py
+++ article/views.py
@@ -3,21 +3,35 @@ from django.http.response import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
 from django.shortcuts import render_to_response
+from article.models import Article, Comments
+
 
 # Create your views here.
 
-#def basic_article_one(request):
-    #view = "basic_article_one"
-    #html = "<html><body>This is %s view</body></html>" % view
 
-    #return HttpResponse(html)
 
 def template_one(request):
-    view = "Name"
+    view = "Article title"
     return render_to_response('articles/article/article_view.html', {'name': view})
 
-    # view = "basic_article_Template"
-    # t = get_template('articles/article/article_view.html')
-    # html = t.render(Context({'name': view}))
-    # return HttpResponse(html)
-#l', {'cart': cart})
+
+# def template_two(request):
+#     view = "basic_article_two"
+#     html = "<html><body>This is %s view</body></html>" % view
+#     return HttpResponse(html)
+#
+# def template_tree(request):
+#     view = "basic_article_Template"
+#     t = get_template('articles/article/article_view.html')
+#     html = t.render(Context({'name': view}))
+#     return HttpResponse(html)
+
+def articles(request):
+    return render_to_response('articles/article/articles.html', {'articles': Article.objects.all()})
+
+
+def comments(request, article_id=1):
+    return render_to_response('articles/article/article.html', {
+        'article': Article.objects.get(id=article_id),
+        'comments': Comments.objects.filter(comments_article_id=article_id)
+    })
